e1186f6edf8393cab5feb8b9cc2e5526ef437c25
Quokkaaa <rktrkdml@gmail.com>
2021-12-10 22:27:12 +0900

    feat: 문제풀이완료 및 README 파일 추가
    * 문제풀이 코드 구현
    * README에 룰 작성완료

--- 2016Year/main.swift
+++ 2016Year/main.swift
@@ -21,33 +21,47 @@ a    b    result
 """
 
 func solution(_ a:Int, _ b:Int) -> String {
-    let months = [1,2,3,4,5,6,7,8,9,10,11,12]
-    let yearDividedByMonth = [31,29,31,30,31,30,31,31,30,31,30,31]
-    let dayOfTheWeek = ["SUN","MON","TUE","WED","THU","FRI","SAT"]
-    let dayOFTheWeekCounter = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
-    var month = ""
-    var day = ""
-    for i in 0..<months.count {
-        if months[i] == a, (1 <= a && a <= 12){
-            month += "\(months[i])"
-        } else {
-            print("a는 1~12숫자로 이내로 입력해주세요")
-        }
+    guard (1 <= a && a <= 12), (1 <= b && b <= 31) else {
+        return "월은 1~12이내로, 일은 1~31이내로 입력해주세요. 단, 2월은 29일까지입니다."
     }
-    // 5월 24일 = 176 경우
-    
-    // 방법1 - 1월부터 날짜를 계산한 176일을 2016년도가 1월1일이 금요일로 시작하니 금~목 까지의 배열수로 카운트해서 값을 추출한다.
+    let yearDividedByMonth = [31,29,31,30,31,30,31,31,30,31,30,31]
+    let dayOfTheWeekCounter = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
+    var sumDays = 0
+    // 5월 24일 = 총 데이수가 176인 경우
+    // 방법1 - 1월부터 날짜를 계산한 176일을 2016년도가 1월 1일이 금요일로 시작하니 금~목 까지의 배열 수로 카운트해서 값을 추출한다.
     
     //방법2
     // 1. 총합 Day수가 7미만 인 경우의 로직
-    
     // 1-2, 0=목요일, 1=금, 2=토, 3=일, 4=월, 5=화, 6=수 로 한정해서 요일을 추출한다.
-    
-    // 2. 총합 Day수가 7이상 인 경우의 로직
-    // 2-1, yearDividedByMonth[0~4]의 수를 더한다.
-    
+    if a == 1, (1 <= b && b < 7) {
+        sumDays += b
+    } else {
+        // 2. 총합 Day수가 7이상 인 경우의 로직
+        // 2-1, yearDividedByMonth[0~4]의 수를 더한다.
+        sumDays += b
+        for i in 0...a-2 {
+            sumDays += yearDividedByMonth[i]
+        }
+    }
     // 2-2, 176을 7로 나눈다
-    
     // 2-3, 나머지에 따라 요일을 값을 할당한다.
-    return " "
+    switch sumDays % 7 {
+    case 0:
+        return dayOfTheWeekCounter[0]
+    case 1:
+        return dayOfTheWeekCounter[1]
+    case 2:
+        return dayOfTheWeekCounter[2]
+    case 3:
+        return dayOfTheWeekCounter[3]
+    case 4:
+        return dayOfTheWeekCounter[4]
+    case 5:
+        return dayOfTheWeekCounter[5]
+    case 6:
+        return dayOfTheWeekCounter[6]
+    default:
+        return "오류"
+    }
 }
+print(solution(1, 4))
