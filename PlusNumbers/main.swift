6f47afc406081d09fa66de186e9a1172e57ec1dd
Quokkaaa <rktrkdml@gmail.com>
2021-12-30 23:32:19 +0900

    refactor: 동작가능코드로 구현

--- PlusNumbers/main.swift
+++ PlusNumbers/main.swift
@@ -1,9 +1,6 @@
-//
 //  main.swift
 //  PlusNumbers
-//
 //  Created by LIMGAUI on 2021/12/29.
-//
 
 import Foundation
 
@@ -11,34 +8,29 @@ import Foundation
 0 <= A <= 99
 - 10 보다 작을 경우 앞에 0 을 붙여 두자리수로 만들고 각 자리의 숫자를 더한다.
 - 그다음 주어진 수의 가장 오른쪽 수와 구한 합의 가장 오른쪽자리수를 이어 붙인다. 1, 0 = 10
-
 """
 
 func plusCycle(_ number: Int) -> Int {
-    guard 0 <= number && number <= 99 else { return 0 }
     // 0부터 99필터 또는 0붙임 조건
-    var usingNumbers = [String]()
-    var rightNumber = 0
+    guard 0 <= number && number <= 99 else { return 0 }
+    // 연산하고 값을 다룰때 사용할 저장변수들
+    var numbers = [String]()
     var sumNumber = 0
-    
+    // 10미만일 경우 두자리수로 만들어주는 조건
     if number < 10 {
-        usingNumbers.append((String(number) + "0"))
+        numbers.append((String(number) + "0"))
     } else {
-        usingNumbers.append((String(number)))
+        numbers.append((String(number)))
     }
     // 숫자를 개별로 분리한 후 numbers에 담는다.
-    
-    var numbers = usingNumbers.filter { number -> Bool in
-        if let unwrappedNumber = Int(number) {
-            sumNumber += unwrappedNumber
-            rightNumber = unwrappedNumber
-        }
-        return Int(number) != 0
-    }
+    var numberArray = Array(String(numbers[0]))
+    numbers.removeFirst()
+    numberArray.forEach{ number in numbers.append(String(number)) }
     // 숫자 합을 sum에 담는다.
-    
+    numbers.forEach { number in sumNumber += Int(number) ?? 0 }
+    var sumRightNumber = Array(String(sumNumber))
     // 주어진 오른쪽 숫자와 합의 오른쪽 수를 붙여 lastOperationValue에 담는다.
-    let lastOperationValue = String(sumNumber) + String(numbers.removeLast())
+    let lastOperationValue = String(numberArray.removeLast()) + String(sumRightNumber.removeLast())
     
     if let result = Int(lastOperationValue) {
         return result
@@ -46,22 +38,4 @@ func plusCycle(_ number: Int) -> Int {
         return 0
     }
 }
-
-let test = 34
-// array값이 왜 분리된 요소들로 나오는지.
-let array = Array(String(test))
-// String -> Int 로 형변환하는 방법
-
-// String.Element오류가 왜그런지 알아보기
-print(plusCycle(26))
-
-let string = "Hello!"
-let arrayStrings = Array(String(string))
-
-//var numbers = Array(String(number))
-//// 숫자 합을 sum에 담는다.
-//
-//numbers.map { (number: String.Element) -> Int in
-//    var intNumbers = 0
-//    return intNumbers
-//}
+print(plusCycle(98))
